2ebb1362d95681d25651494646974e4438e79f7b
Alvin Balmeo <albalmeo@gmail.com>
2019-10-07 19:43:06 -0700

    Adding optimized solution

--- Two_Sum.py
+++ Two_Sum.py
@@ -2,13 +2,36 @@
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 # Time complexity : O(n^2)
+# Brute force method
 
 
-def two_sum(num_list, target):
-    for i in range(num_list):
-        for j in range(num_list):
+def two_sum_On2(num_list, target):
+    length = len(num_list)
+    for i in range(length):
+        for j in range(length):
             value = num_list[i] + num_list[j]
 
             if target == value and i != j:
+                print(num_list[i], num_list[j])
                 return True
     return False
+
+
+# Time complexity : O(n)
+# Optimized
+def two_sum_On(num_list, target):
+    # store complement in dictionary or hashmap
+    complementMap = dict()
+    # loop through numbers
+    for i in range(len(num_list)):
+        num = num_list[i]
+        # find complement of number for target
+        complement = target - num
+        print('num', num)
+        print('complement', complement)
+        # if number in complementMap, then complement exists that was looped through before
+        if num in complementMap:
+            return [complementMap[num], i]
+        # if number not in complementMap, then add to comlpement map
+        else:
+            complementMap[complement] = i
